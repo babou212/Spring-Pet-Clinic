8b9eb3a10a36495dc37177e89e09cc8e76bad0a9
dylanc <dylanpab1@gmail.com>
2022-07-06 16:43:46 +0100

    Implemented map based services. closes #11

--- pet-clinic-data/src/main/java/com/example/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/PetService.java
@@ -2,7 +2,5 @@ package com.example.springpetclinic.services;
 
 import com.example.springpetclinic.model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/com/example/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/VetService.java
@@ -2,7 +2,5 @@ package com.example.springpetclinic.services;
 
 import com.example.springpetclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet, Long> {
 }
