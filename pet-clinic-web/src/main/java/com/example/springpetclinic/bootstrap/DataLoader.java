d1305f6d0f7acd2604d59469abfa53c85e0a46e0
Dylan Cree <dylanpab1@gmail.com>
2022-07-11 14:18:22 +0100

    Added Owners List and updated controller. Closes #16

--- pet-clinic-web/src/main/java/com/example/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.example.springpetclinic.model.Owner;
 import com.example.springpetclinic.model.Vet;
 import com.example.springpetclinic.services.OwnerService;
 import com.example.springpetclinic.services.VetService;
-import com.example.springpetclinic.services.map.OwnerMapService;
-import com.example.springpetclinic.services.map.VetMapService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +13,10 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader(){
-        ownerService = new OwnerMapService();
-        vetService = new VetMapService();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
