4c6d0b2ff760a558849940be7c639abf4caf2ba8
Dylan Cree <dylanpab1@gmail.com>
2022-07-17 11:39:57 +0100

    Added static resources from spring pet clinic. closes #17

--- pet-clinic-data/src/main/java/com/example/springpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/map/AbstractMapService.java
@@ -23,7 +23,7 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
             }
             map.put(object.getId(), object);
         }else {
-            throw  new RuntimeException("Object cannot be null");
+            throw new RuntimeException("Object cannot be null");
         }
         return object;
     }
