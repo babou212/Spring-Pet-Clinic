71d20b834dddae9dd2d7343680b712bcd10f1d43
Dylan Cree <dylanpab1@gmail.com>
2022-08-13 14:32:09 +0100

    Added vet json endpoint

--- pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/VetController.java
@@ -1,11 +1,16 @@
 package com.example.springpetclinic.controllers;
 
+import com.example.springpetclinic.model.Vet;
 import com.example.springpetclinic.services.VetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,5 +24,9 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
